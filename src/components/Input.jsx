b3efbd207c7ddb7a8471e2b4ef4ba798bebc888e
mao157722 <48387677+mao157722@users.noreply.github.com>
2024-05-26 21:24:59 +0700

    init

---
+++ src/components/Input.jsx
@@ -0,0 +1,17 @@
+export default function Input({ label, invalid, ...props }) {
+  let labelClass = 'block mb-2 text-xs font-bold tracking-wide uppercase text-stone-300'
+  let inputClass = 'w-full px-3 py-2 leading-tight border rounded shadow'
+  if(invalid) {
+    labelClass += ' text-red-400';
+    inputClass += ' text-red-500 bg-red-100 border-red-300';
+  } else {
+    labelClass += ' text-stone-300';
+    inputClass += ' text-gray-700 bg-stone-300';
+  }
+  return (
+    <p>
+      <label className={labelClass}>{label}</label>
+      <input className={inputClass} {...props} />
+    </p>
+  );
+}
